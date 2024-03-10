82fcd2286bafc1d44330564afca0b027eeb4111f
DaMaTu <matthias.jarvet@gmail.com>
2024-03-10 19:44:50 +0200

    game.py

--- game.py
+++ game.py
@@ -1,13 +1,25 @@
 import pygame
-pygame.init()
 
-screen = pygame.display.set_mode([800, 500])
+from settings import Settings
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def run_game():
+    pygame.init()
+
+    gm_settings = Settings()
+
+    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+    pygame.display.set_caption(gm_settings.caption)
+
+    running = True
+    while running:
+        screen.fill(gm_settings.big_color)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        pygame.display.flip()
+    pygame.quit()
+    print(running)
     
-    pygnae.display.flip()
-pygame.quit()
+run_game()
