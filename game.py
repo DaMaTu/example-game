4d5df247f305654bfb63b05bc54fc8503367fa02
DaMaTu <matthias.jarvet@gmail.com>
2024-03-10 19:54:57 +0200

    Setup game window

--- game.py
+++ game.py
@@ -1,25 +1,29 @@
-import pygame
+# Simple pygame program
 
+# Import and initalize the pygame libary
+import pygame
 from settings import Settings
 
 def run_game():
     pygame.init()
-
     gm_settings = Settings()
 
+    # Set up the drawing window
     screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
     pygame.display.set_caption(gm_settings.caption)
 
+    # Run until user asks to quit
     running = True
     while running:
         screen.fill(gm_settings.big_color)
-        
+        # Did the user click the window close button?
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
+        # Flip the display
         pygame.display.flip()
+    # Done! Time to quit.    
     pygame.quit()
-    print(running)
-    
+
 run_game()
