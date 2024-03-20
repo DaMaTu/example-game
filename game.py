86e527da1f2db63002225379a16fff386d8007d1
DaMaTu <matthias.jarvet@gmail.com>
2024-03-20 13:30:41 +0200

    As a player I can see and move my character (player) at screen [Delivers #187224039]

--- game.py
+++ game.py
@@ -3,11 +3,17 @@
 # Import and initalize the pygame libary
 import pygame
 from settings import Settings
+<<<<<<< HEAD
+=======
+from player import Player
+import game_functions as gf
+>>>>>>> setup-game-window
 
 def run_game():
     pygame.init()
     gm_settings = Settings()
 
+<<<<<<< HEAD
     # Set up the drawing window
     screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
     pygame.display.set_caption(gm_settings.caption)
@@ -25,5 +31,19 @@ def run_game():
         pygame.display.flip()
     # Done! Time to quit.    
     pygame.quit()
+=======
+    
+    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+    pygame.display.set_caption(gm_settings.caption)
+
+
+
+    player = Player(screen)
+
+    while True:
+        gf.check_events(player)
+        player.update()
+        gf.update_screen(gm_settings, screen, player)
+>>>>>>> setup-game-window
 
 run_game()
--- settings.py
+++ settings.py
@@ -1,11 +1,19 @@
 class Settings:
     """Class for game settings"""
     
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> setup-game-window
     def __init__(self):
         """Initialize game settings"""
         # Screen settings
         self.screen_width = 800
         self.screen_height = 600
+<<<<<<< HEAD
         self.big_color = (0, 0, 139)
+=======
+        self.bg_color = (0, 0, 139)
+>>>>>>> setup-game-window
         self.caption = 'Bubble bluster'
