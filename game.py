b1cd8e42e182c21b735dcd391a0c176ce2c41f59
DaMaTu <matthias.jarvet@gmail.com>
2024-03-26 15:05:36 +0200

    Add bubble and it's game logic [Delivers #18727645]

--- game.py
+++ game.py
@@ -3,47 +3,29 @@
 # Import and initalize the pygame libary
 import pygame
 from settings import Settings
-<<<<<<< HEAD
-=======
 from player import Player
+from bubble import Bubble
 import game_functions as gf
->>>>>>> setup-game-window
 
 def run_game():
     pygame.init()
     gm_settings = Settings()
 
-<<<<<<< HEAD
     # Set up the drawing window
     screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
     pygame.display.set_caption(gm_settings.caption)
 
-    # Run until user asks to quit
-    running = True
-    while running:
-        screen.fill(gm_settings.big_color)
-        # Did the user click the window close button?
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        
-        # Flip the display
-        pygame.display.flip()
-    # Done! Time to quit.    
-    pygame.quit()
-=======
-    
-    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
-    pygame.display.set_caption(gm_settings.caption)
-
-
-
+    # Instantiate player
     player = Player(screen)
-
+    
+    # Create groups to hold bubbles
+    bubbles = pygame.sprite.Group()
+    
+   # Run until the user asks to quit
     while True:
-        gf.check_events(player)
+        gf.check_events(gm_settings, screen, player, bubbles)
         player.update()
-        gf.update_screen(gm_settings, screen, player)
->>>>>>> setup-game-window
+        bubbles.update()
+        gf.update_screen(gm_settings, screen, player, bubbles)
 
 run_game()
--- settings.py
+++ settings.py
@@ -1,19 +1,14 @@
 class Settings:
     """Class for game settings"""
     
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> setup-game-window
     def __init__(self):
         """Initialize game settings"""
         # Screen settings
         self.screen_width = 800
         self.screen_height = 600
-<<<<<<< HEAD
-        self.big_color = (0, 0, 139)
-=======
         self.bg_color = (0, 0, 139)
->>>>>>> setup-game-window
         self.caption = 'Bubble bluster'
+        
+        self.bubble_min_r =10
+        self.bubble_max_r = 50
